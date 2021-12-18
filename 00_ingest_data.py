f918aa20d66e042b16297b04765922243b6a5088
hzeig <hzeig>
2021-12-18 17:36:27 +0000

    union troubleshooting

--- 00_ingest_data.py
+++ 00_ingest_data.py
@@ -45,11 +45,58 @@ for item in dbutils.fs.ls(mntPath):
         df = recieve_data(file)
         df = process_citibike_data(df)
         dfs.append(df)
-        
-pd.concat(dfs)
 
-spark.sql(f"""
-CREATE TABLE citibike_full
-USING DELTA
-LOCATION "dailyPath"
-""")
+# COMMAND ----------
+
+df.printSchema()
+
+# COMMAND ----------
+
+a = unionDF.union(dfs[0])
+b = a.union(dfs[1])
+print(a.count())
+print(b.count())
+
+# COMMAND ----------
+
+from pyspark.sql.types import *
+
+schema = StructType([
+  StructField('tripduration_min', DoubleType(), True),
+  StructField('starttime', TimestampType(), True),
+  StructField('endtime', TimestampType(), True),
+  StructField('date', DateType(), True),
+  StructField('day', IntegerType(), True),
+  StructField('startID', LongType(), True),
+  StructField('startlat', DoubleType(), True),
+  StructField('startlon', DoubleType(), True),
+  StructField('endID', LongType(), True),
+  StructField('endlat', DoubleType(), True),
+  StructField('endlon', DoubleType(), True),
+  StructField('distance_start-end', DoubleType(), True),
+  StructField('bikeid', LongType(), True),
+  StructField('usertype', StringType(), True),
+  StructField('userbirth', LongType(), True),
+  StructField('userage', LongType(), True),
+  StructField('usergender', LongType(), True)
+])
+
+unionDF = spark.createDataFrame([],schema)
+for item in dfs:
+    unionDF = unionDF.union(item)
+
+# COMMAND ----------
+
+# spark.sql(f"""
+# CREATE TABLE citibike_full
+# USING DELTA
+# LOCATION "projectPath" + "dailyPath"
+# """)
+
+# COMMAND ----------
+
+unionDF.count()
+
+# COMMAND ----------
+
+
