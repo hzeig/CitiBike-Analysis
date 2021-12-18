095b28f68b2e7b2d76d62f57a0c948ddcc8b5d21
hzeig <hzeig>
2021-12-18 03:19:41 +0000

    adding conf

---
+++ includes/configuration.py
@@ -0,0 +1,40 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ####Define Data Paths
+# MAGIC 
+# MAGIC Practice pipelining with user-specific directories
+
+# COMMAND ----------
+
+# TODO
+username = 'hadar'
+experiment_id = 3260145608445930
+
+# COMMAND ----------
+
+projectPath = f"/CitiBike/{username}/data/"
+s3Path = "s3a://{0}:{1}@{2}".format("AKIAVPSFD5OHVN5FJMWQ",
+                                    "ZMe70t9oyebrmBgopaPNtvcdXDolAnmR3Rtsax3I", 
+                                    "citibike-capstone-hadar")
+mntPath     = "/mnt/data/"
+dailyPath   = projectPath + "daily/"
+aggPath     = projectPath + "agg/"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Configure Database**
+
+# COMMAND ----------
+
+spark.sql(f"CREATE DATABASE IF NOT EXISTS citibike_{username}")
+spark.sql(f"USE citibike_{username}")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Import Utility Functions**
+
+# COMMAND ----------
+
+# MAGIC %run ./utilities
